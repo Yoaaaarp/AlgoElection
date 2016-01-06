0f82c970ae1ac3f054c5aa659ec5364ec7d9a4cf
Marc Pellet <marc.pellet@heig-vd.ch>
2016-01-06 22:17:52 +0100

    ajout enum pour les messages

---
+++ src/Message.java
@@ -0,0 +1,14 @@
+/**
+ * Author  : Marc
+ * Project : AlgoElection
+ * File    : Message.java
+ * Date    : 6 janv. 2016
+ */
+
+/**
+ * @author Marc
+ *
+ */
+public enum Message {
+	ELECTION, RESULTAT, BONJOUR
+}
